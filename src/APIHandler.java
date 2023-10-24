2423083ca776867aa9b1e189a9e509cc547261c3
PickGreta <pickermann.greta@gmail.com>
2023-10-24 21:59:16 +0200

    City label

--- src/APIHandler.java
+++ src/APIHandler.java
@@ -5,13 +5,13 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class APIHandler {
-
-    void callAPI() {
-        //String apiKey = "8044c498e29af2db48f9a3e41021aa7d";
-        //String apiURL = "http://api.weatherstack.com/current?access_key=8044c498e29af2db48f9a3e41021aa7d&query=Budapest";
+git
+    void callAPI(String cityName) {
+        String apiKey = "8044c498e29af2db48f9a3e41021aa7d";
+        String apiURL = "http://api.weatherstack.com/current?access_key=" + apiKey + "&query=" + cityName;
 
         try {
-            URL url = new URL("http://api.weatherstack.com/current?access_key=8044c498e29af2db48f9a3e41021aa7d&query=Budapest");
+            URL url = new URL(apiURL);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,9 +21,15 @@ public class Main {
 
         JButton searchButton = new JButton("Search");
 
+        JLabel cityLabel = new JLabel();
+
+        JLabel temperatureLabel = new JLabel();
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10); //padding
         gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        // Align the search components at the top
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 2;
@@ -34,10 +42,37 @@ public class Main {
         gbc.gridx = 1; //Move to the next column
         panel.add(searchButton, gbc);
 
+        // Display the city's name below the search components
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        panel.add(cityLabel, gbc);
+
+        gbc.gridy = 3;
+        panel.add(temperatureLabel, gbc);
+
         frame.add(panel);
         frame.setVisible(true);
 
-        APIHandler apihandler = new APIHandler();
-        apihandler.callAPI();
+        APIHandler apiHandler = new APIHandler();
+
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String cityName = searchBar.getText();
+                if (!cityName.isEmpty()) {
+                    apiHandler.callAPI(cityName);
+
+                    cityLabel.setText("City: " + cityName);
+
+                    String temperature = getTemperatureFromAPI(cityName);
+                    temperatureLabel.setText("Temperature: " + temperature);
+                }
+            }
+        });
+    }
+
+    private static String getTemperatureFromAPI(String cityName) {
+        return "19°C";
     }
 }
